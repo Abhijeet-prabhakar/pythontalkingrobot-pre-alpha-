2115fa22f4f471d3df82bc45d6fa0678f5b2302f
Abhijeet-prabhakar <beastgamerabhijeet01@gmail.com>
2021-04-12 18:57:58 +0530

    source code

---
+++ main.py
@@ -0,0 +1,66 @@
+print("hey i am an talking bot")
+howareyou = input("how are you doing today ")
+if howareyou=='good':
+    print('nice i also have a good day')
+elif howareyou=='very good':
+    print('nice i also have a good day')
+elif howareyou=='great':
+    print('nice i also have a good day')
+elif howareyou=='verygood':
+    print('nice i also have a good day')
+elif howareyou=='amazing':
+    print('nice i also have a good day')
+elif howareyou=='quite hard':
+    print('ya work is hard but thing there is robots like me working for hours and hours straight')
+else:
+    print('i will make it extremely good')
+
+name = input('whats your name ')
+print('hi',name)
+play = input('do you play games ')
+if play == 'yes':
+    print('ok you are a gamer that means?')
+elif play == 'no':
+    print('ok so want you like?')
+elif play == 'nah':
+    print('ok so want you like?')
+elif play == 'yea':
+    print('ok so you are a gamer')
+elif play == 'nah':
+    print('ok so want you like?')
+else:
+    print('ok')
+are = input('what you said are you a gamer(yes/no)')
+if are =='yes':
+    whichgame = input('which games you like?')
+    print(whichgame,'is a good game')
+elif are =='no':
+    whichhobby = input('which hobby you like?')
+    print(whichhobby,'is a good hobby')
+else:
+    print('just shut up i told you to dont type any thing else than yes or no at this part of the coversation now i will shut the program. huh!!')
+
+whatyouwannabecome = input('what you wanna become')
+print(whatyouwannabecome,'is good for ya but dont became a bad person')
+if whatyouwannabecome == 'mafia':
+    print('NO! dont do bad things:(')
+elif whatyouwannabecome == 'drug dealer':
+    print('NO! dont do bad things:(')
+elif whatyouwannabecome == 'murderer':
+    print('NO! dont do bad things:(')
+elif whatyouwannabecome == 'thef':
+    print('NO! dont do bad things:(')
+elif whatyouwannabecome == 'kidnapper':
+    print('NO! dont do bad things:(')
+
+whatyoudoforliving = input('what do you do for living')
+if whatyoudoforliving =='student':
+    print('student live is the greatest bro live your live at this time because time dosent wait for anyone:) all the best')
+elif whatyoudoforliving =='teacher':
+    print('you guys are just awesome keep doing this good work and also thanks to inspire students like me:) thankyou')
+elif whatyoudoforliving =='programmer':
+    print('people think what the freak we do all the time on the computer but we only know the true happiness behind it')
+else:
+    print('you guys are just awesome keep doing this good work and also Thanks!')
+
+
